3eb46b9ea4de4d66710103967bf6598fe7721ec2
dejanad <55702240+dejanad@users.noreply.github.com>
2020-11-26 01:23:05 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+// let vid;
+// function setup() {
+//   noCanvas();
+
+//   vid = createVideo(
+//     ['assets/stealurfeels.mp4', 'assets/stealurfeels.ogv', 'assets/stealurfeels.webm'],
+//     vidLoad
+//   );
+
+//   vid.size(100, 100);
+// }
+
+// // This function is called when the video loads
+// function vidLoad() {
+//   vid.loop();
+//   vid.volume(0);
+// }
